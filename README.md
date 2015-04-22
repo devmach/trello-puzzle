0ae5ab4f7c26fa352d619bd14c9f35b00cd38173
Aydin /dev/mach Ulf <root@devmach.com>
2015-04-22 11:26:16 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+Recently, I came across one of the trello's job listings. To
+apply, one has to solve a small puzzle and send an email with
+a "magic word" in subject. I think it works a better fizz-buzz
+test.
+
+While I was having a coffee break, I wrote a simple solver in
+JS. It was fun. While it's easy to solve that puzzle for any
+developer, I just wanted to share it.
+
+You can see the original listing at
+https://trello.com/jobs/windows-mobile-developer
---
+++ puzzle.js
@@ -0,0 +1,31 @@
+var TrelloPuzzle = {
+  _letters : 'acdegilmnoprstuw',
+
+  decode:  function(num) {
+    var s = '';
+    while(num > 37) {
+      q = num % 37;
+      s += this._letters.charAt(q)
+      num = (num - q) / 37;
+    }
+    return s.split('').reverse().join('');
+  },
+
+  encode: function(str) {
+    var h = 7;
+    var letters = 'acdegilmnoprstuw';
+    for(var i = 0; i < str.length; ++i) {
+      h *= 37;
+      h += this._letters.indexOf(str[i]);
+    };
+
+    return h;
+  }
+};
+
+
+console.log("680131659347 === ", TrelloPuzzle.decode(680131659347));
+console.log("leepadg === ", TrelloPuzzle.encode("leepadg"));
+
+console.log("Encoding Test Passes: ", TrelloPuzzle.encode("leepadg") === 680131659347);
+console.log("Decoding Test Passes: ", TrelloPuzzle.decode(680131659347) === "leepadg");
